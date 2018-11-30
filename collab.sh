5a6cc7f264265a101fee226376315a389c04c86c
Marcus Price <marcusprice88@gmail.com>
2018-11-29 20:42:39 -0800

    created collab.sh

---
+++ collab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Collaborators Assignment
+# ITC136 - Fall 2018
+# Marcus Price
+# Eddy Matlock-Mahon
+# Jennifer Villacis
+
+echo "The 9000 series is the most reliable computer ever made. No 9000 computer has 
+ever made a mistake or distorted information. We are all, by any practical definition 
+of the words, foolproof and incapable of error."
