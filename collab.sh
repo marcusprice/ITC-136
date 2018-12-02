4d1f9cb85fb3bc86857ab27abcf2edb141e445ae
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2018-12-02 00:04:52 -0800

    updates collab

--- collab.sh
+++ collab.sh
@@ -9,3 +9,6 @@
 echo "The 9000 series is the most reliable computer ever made. No 9000 computer has 
 ever made a mistake or distorted information. We are all, by any practical definition 
 of the words, foolproof and incapable of error."
+echo "Technology will never replace great teachers, but technology in the hands of great teachers
+is transformational."
+
